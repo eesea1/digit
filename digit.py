7538c20c93722ee98c18199d05e86ec71384fc90
eesea1 <66675182+eesea1@users.noreply.github.com>
2023-02-04 21:39:19 +0300

    Add files via upload

--- digit.py
+++ digit.py
@@ -1,14 +1,10 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 from tensorflow import keras
 import tensorflow as tf
 import numpy as np
 
-def cnn_digits_predict(model, image_file):
+def predict(model, image_file):
    image_size = 28
-   img = keras.preprocessing.image.load_img(image_file,
-target_size=(image_size, image_size), color_mode='grayscale')
+   img = keras.preprocessing.image.load_img(image_file,target_size=(image_size, image_size), color_mode='grayscale')
    img_arr = np.expand_dims(img, axis=0)
    img_arr = 1 - img_arr/255.0
    img_arr = img_arr.reshape((1, 28, 28, 1))
@@ -16,5 +12,5 @@ target_size=(image_size, image_size), color_mode='grayscale')
    result = model.predict_classes([img_arr])
    return result[0]
 
-model = tf.keras.models.load_model('cnn_digits_28x28.h5')
-print(cnn_digits_predict(model, '1.png'))
+model = tf.keras.models.load_model('mnist.h5')
+print(predict(model, '2.png'))
