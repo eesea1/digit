06897e08058f9d6e1eae1935c255bd056039854d
eesea1 <66675182+eesea1@users.noreply.github.com>
2023-02-13 18:37:23 +0300

    Add files via upload

--- digit.py
+++ digit.py
@@ -2,15 +2,15 @@ from tensorflow import keras
 import tensorflow as tf
 import numpy as np
 
-def predict(model, image_file):
-   image_size = 28
-   img = keras.preprocessing.image.load_img(image_file,target_size=(image_size, image_size), color_mode='grayscale')
-   img_arr = np.expand_dims(img, axis=0)
-   img_arr = 1 - img_arr/255.0
-   img_arr = img_arr.reshape((1, 28, 28, 1))
 
-   result = model.predict_classes([img_arr])
-   return result[0]
+def predict(model, img):
+   img = img.resize((28, 28))
+   img = img.convert('L')
+   img = np.array(img)
+   img = img.reshape(1, 28, 28, 1)
+   img = img / 255.0
+   res = model.predict([img])[0]
+   return np.argmax(res), max(res)
 
 model = tf.keras.models.load_model('mnist.h5')
 print(predict(model, '2.png'))
